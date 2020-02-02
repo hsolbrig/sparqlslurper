14324c48ab76e6a5222366b8453deba3f5df9abd
hsolbrig <solbrig@jhu.edu>
2020-02-03 05:37:39 +1100

    Pull lock file out of build

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+import sys
 
 from setuptools import setup
+
+if sys.version_info < (3, 7):
+    raise Exception("Python 3.7 or higher is required. Your version is %s." % sys.version)
 
 setup(
     setup_requires=['pbr'],
